0603e2c52c70ddca33d01910131209015fe1fd0b
DaFulk <falktennstedt@gmail.com>
2021-12-16 10:20:55 +0100

    Continued Categories and Products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,15 +5,74 @@ import {
 } from "@material-ui/icons";
 import styled from "styled-components";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0, 0.2);
+  z-index:3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s ease;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
-const Circle = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Icon = styled.div``;
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index:2;
+
+/*   Object-fit helps with pictures that are two wide … but it needs both: width & height;
+  width: 100%;
+  object-fit: cover; */
+`;
+
+
+
+const Icon = styled.div`
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;  
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  cursor: pointer;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+    ;
+  }
+`;
 
 const Product = ({ item }) => {
   return (
