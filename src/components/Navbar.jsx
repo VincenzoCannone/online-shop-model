da76e0301a09e2346728e6352fca2be67199121b
DaFulk <falktennstedt@gmail.com>
2022-01-10 19:24:52 +0100

    Started optimizing the CSS stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,76 +1,8 @@
+import React from "react";
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
-import React from "react";
-import styled from "styled-components";
-import { mobile } from "../responsive";
-
-const Container = styled.div`
-  height: 60px;
-  ${mobile({ height: "50px" })}
-`;
-
-const Wrapper = styled.div`
-  padding: 10px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  ${mobile({ padding: "10px 0" })}
-`;
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
-
-const Language = styled.span`
-  font-size: 14px;
-  cursor: pointer;
-  ${mobile({ display: "none" })}
-`;
-
-const SearchContainer = styled.div`
-  border: 0.5px solid lightgray;
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-  padding: 5px;
-  ${mobile({ margin: "auto" })}
-`;
-
-const Input = styled.input`
-  border: none;
-  ${mobile({ width: "55px" })}
-`;
-
-const Logo = styled.h1`
-  font-weight: bold;
-  ${mobile({ fontSize: "20px" })}
-`;
-
-const Center = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  z-index: 2;
-`;
-
-//? why flex:2 is without ""???
-const Right = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  ${mobile({ flex: 1.5, marginRight: "5px" })}
-`;
-
-const MenuItem = styled.div`
-  font-size: 14px;
-  cursor: pointer;
-  margin-left: 25px;
-  ${mobile({ fontSize: "15px", marginLeft: "10px" })}
-`;
+import { Container, Wrapper, Left, Language, SearchContainer, Input, Logo, Center, Right, MenuItem } from './styles/StyledNavbar';
+// import { green } from "@material-ui/core/colors";
 
 //! Badge number (4) we cant scale it 'badgeContent={4}'
 
