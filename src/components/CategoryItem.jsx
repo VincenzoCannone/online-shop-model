5b30f3c37e861ecfc6517684725866ccc3d4064d
enzo <cannone.vin@gmail.com>
2022-01-19 23:14:15 +0100

    filter-fetcht-products-collection

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,27 +1,46 @@
-import { Container, Image, Info, Title, Button } from './styles/StyledCategoryItem'
-import { useState } from 'react';
-import { letterSpacing } from '@mui/system';
+import {
+  Container,
+  Image,
+  Info,
+  Title,
+  Button,
+} from "./styles/StyledCategoryItem";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { letterSpacing } from "@mui/system";
 
 const CategoryItem = ({ item }) => {
-
-  const [theCSS, setCSS] = useState({ color: '#fff', spacing: '0', shadow: "2px 2px 8px #888" });
+  const [theCSS, setCSS] = useState({
+    color: "#fff",
+    spacing: "0",
+    shadow: "2px 2px 8px #888",
+  });
 
   const changeIt = () => {
-    setCSS({ color: '#fff', spacing: '0.5rem', shadow: "2px 2px 8px #333" });
-  }
+    setCSS({ color: "#fff", spacing: "0.5rem", shadow: "2px 2px 8px #333" });
+  };
 
   const setBack = () => {
-    setCSS({ color: '#fff', spacing: '0', shadow: "2px 2px 8px #888" });
-  }
+    setCSS({ color: "#fff", spacing: "0", shadow: "2px 2px 8px #888" });
+  };
 
   return (
     <Container onMouseOver={changeIt} onMouseLeave={setBack}>
+      <Link to={`/products/${item.cat}`}></Link>
       <Image src={item.img} />
       <Info>
-        <Title style={{ color: theCSS.color, letterSpacing: theCSS.spacing, textShadow: theCSS.shadow }}>{item.title}</Title>
+        <Title
+          style={{
+            color: theCSS.color,
+            letterSpacing: theCSS.spacing,
+            textShadow: theCSS.shadow,
+          }}
+        >
+          {item.title}
+        </Title>
         <Button>SHOP NOW</Button>
       </Info>
-    </Container >
+    </Container>
   );
 };
 
